a4923a219c8f8c671dde693f2c8486dd8d0e1ffc
Thomas Alrek <thomas@alrek.no>
2023-02-23 15:10:03 +0100

    Make default model refresh on save

--- src/Model.php
+++ src/Model.php
@@ -3,15 +3,18 @@
 namespace Netflex\Database;
 
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+
 use Netflex\Database\Concerns\GeneratesName;
 use Netflex\Database\Concerns\Publishable;
 use Netflex\Database\Concerns\PublishesRevisions;
+use Netflex\Database\Concerns\RefreshesOnSave;
 
 abstract class Model extends EloquentModel
 {
     use GeneratesName;
     use Publishable;
     use PublishesRevisions;
+    use RefreshesOnSave;
 
     const CREATED_AT = 'created';
     const UPDATED_AT = 'updated';
